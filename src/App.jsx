c90302665791225dba905a6431f0672cadb1df1b
Juan Carlos Ruperto Zarate <juancarloszarate586@gmail.com>
2022-05-20 00:53:55 -0500

    Calculadora funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,269 @@
-import React, { useState } from 'react'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [resultado, setResultado] = useState("")
+  const [inputState, setinputState] = useState({
+    operacion: "",
+    resultado: "",
+  });
 
-  const handleClick = (e) => {
-      setResultado(resultado.concat(e.target.name));
-  }
+  const inicioEstado = JSON.parse(localStorage.getItem("historial")) || [];
+  const [historial, setHistorial] = useState(inicioEstado);
 
-  const limpiar = () => {
-    setResultado("");
-  }
+  const handleInputChange = (event) => {
+    setinputState({
+      ...inputState,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-  const eliminar = () => {
-    setResultado(resultado.slice(0, -1)); //elimina cada numero en uno en uno
-  }
+  const handleNumeroClick = (event) => {
+    setinputState({
+      ...inputState,
+      operacion: inputState.operacion + event.target.value,
+    });
+  };
 
-  const calculador = () => {
-    
-    try {
-      setResultado(eval(resultado).toString());
-    } catch(err){
-      setResultado("Error")
-    } 
-  }
-  
+  const handleOperadorClick = (event) => {
+    setinputState({
+      ...inputState,
+      operacion: inputState.operacion + event.target.textContent,
+    });
+  };
+
+  const handleResultado = () => {
+    var datos = inputState.operacion;
+    var res = eval(datos);
+
+    localStorage.setItem(
+      "historial",
+      JSON.stringify([
+        ...historial,
+        {
+          operacion: datos,
+          resultado: res,
+        },
+      ])
+    );
+    setHistorial([
+      ...historial,
+      {
+        operacion: datos,
+        resultado: res,
+      },
+    ]);
+
+    setinputState({
+      ...inputState,
+      operacion: res,
+      resultado: "",
+    });
+  };
+
+  const handleClickLimpiar = () => {
+    setinputState({
+      ...inputState,
+      operacion: "",
+      resultado: "",
+    });
+  };
+
+  const handleResetHistorial = () => {
+    setHistorial([]);
+    localStorage.setItem("historial", JSON.stringify([]));
+  };
+
+
+
+
   return (
-    <>
-      <div className="contenedor">
-        <form>
-          <input type="text" value={resultado} style={{background: "#d9ffea", width: "215px", height: "120px", margin:"15px" }}/>
-        </form>
-
-        <div className="teclado">
-          <button onClick={limpiar} id="limpiar" className="botonesrojos">Limpiar</button>
-          <button onClick={eliminar} id="eliminar" className="botonesamarillos">C</button>
-          <button name="/" onClick={handleClick} className="botonesamarillos">&divide;</button>
-          <button name="7" onClick={handleClick}>7</button>
-          <button name="8" onClick={handleClick}>8</button>
-          <button name="9" onClick={handleClick}>9</button>
-          <button name="*" onClick={handleClick} className="botonesamarillos">&times;</button>
-          <button name="4" onClick={handleClick}>4</button>
-          <button name="5" onClick={handleClick}>5</button>
-          <button name="6" onClick={handleClick}>6</button>
-          <button name="-" onClick={handleClick} className="botonesamarillos">&ndash;</button>
-          <button name="1" onClick={handleClick}>1</button>
-          <button name="2" onClick={handleClick}>2</button>
-          <button name="3" onClick={handleClick}>3</button>
-          <button name="+" onClick={handleClick} className="botonesamarillos">+</button>
-          <button name="0" onClick={handleClick}>0</button>
-          <button name="." onClick={handleClick}>.</button>
-          <button onClick={calculador} id="resultado" className="botonesamarillos" >=</button>
-
+    <div className="App container mt-4">
+      
+      <center>
+    <div className="row card shadow text-center " id="contenedor" >
+      
+      <div className="row">
+         
+        <div className="mb-3 text-end">
+          <br />
+          <center>
+          <input 
+          style={{background: "#d9ffea"}}
+            id="input"
+            name="input"
+            className="p-2 w-75 text-end shadow"
+            disabled={true}
+            onChange={handleInputChange}
+            value={inputState.operacion}
+            >
+            </input>
+            </center>
         </div>
 
+        <div className="col card-body" style={{width: "200px"}}>
+          <h3 className="text-center animate__animated animate__rubberBand">Historial :)</h3>
+          {
+            historial.length === 0 ?
+            "No hay operaciones aun :("
+            :
+            (
+              <ol>
+                {historial.map((item,index) => {
+                  return(
+                    <li key={index} style={{fontSize: "150%"}}>
+                      {item.operacion} = {item.resultado} &nbsp;
+                    </li>
+                  );
+                })
+                }
+              </ol>
+            )
+          }
+          <br />
+          <button
+            className="btn-lg m-2"
+            style={{width: "100px"}}
+            value="+"
+            onClick={handleResetHistorial}
+            id="colorojo"
+          >Borrar</button>
+        </div>
+            
+        <div className="col card-body" style={{width: "640px"}}>
+          <button
+            className="btn-lg m-2"
+            style={{width: "100px"}}
+            value="+"
+            onClick={handleOperadorClick}
+            id="coloramarillos"
+          >+</button>
+          <button
+            className="btn-lg"
+            style={{width: "100px"}}
+            value="-"
+            onClick={handleOperadorClick}
+            id="coloramarillos"
+          >-</button>
+          <button
+            className="btn-lg m-2"
+            style={{width: "100px"}}
+            value="*"
+            onClick={handleOperadorClick}
+            id="coloramarillos"
+          >*</button>
+          <button
+            className="btn-lg"
+            style={{width: "100px"}}
+            value="/"
+            onClick={handleOperadorClick}
+            id="coloramarillos"
+          >/</button>
+          <br />  
+          <button
+            className="btn-lg m-2"
+            style={{width: "100px"}}
+            value={9}
+            onClick={handleNumeroClick}
+            id="colorazul"
+          >9</button>
+          <button
+            className=" btn-lg"
+            style={{width: "100px"}}
+            value={8}
+            onClick={handleNumeroClick}
+            id="colorazul"
+          >8</button>
+          <button
+            className="btn-lg m-2"
+            style={{width: "100px"}}
+            value={7}
+            onClick={handleNumeroClick}
+            id="colorazul"
+          >7</button>
+           <button
+            className="btn-lg m-2"
+            style={{width: "100px"}}
+            value={"."}
+            onClick={handleNumeroClick}
+            id="coloramarillos"
+          >.</button><br />
+
+          <button
+            className=" btn-lg m-2"
+            style={{width: "100px"}}
+            value={6}
+            onClick={handleNumeroClick}
+            id="colorazul"
+          >6</button>
+          <button
+            className="btn-lg m-2"
+            style={{width: "100px"}}
+            value={5}
+            onClick={handleNumeroClick}
+            id="colorazul"
+          >5</button>
+          <button
+            className="btn-lg "
+            style={{width: "100px"}}
+            value={4}
+            onClick={handleNumeroClick}
+            id="colorazul"
+          >4</button>
+
+          <button
+          className="btn-lg"
+          style={{width: "100px"}}
+          onClick={handleResultado}
+          value="="
+          id="coloramarillos"
+          >=</button> <br />  
+
+          <button
+            className="btn-lg m-2"
+            style={{width: "100px"}}
+            value={3}
+            onClick={handleNumeroClick}
+            id="colorazul"
+          >3</button> 
+          
+          <button
+            className=" btn-lg"
+            style={{width: "100px"}}
+            value={2}
+            onClick={handleNumeroClick}
+            id="colorazul"
+          >2</button> 
+          <button
+            className="btn-lg m-2"
+            style={{width: "100px"}}
+            value={1}
+            onClick={handleNumeroClick}
+            id="colorazul"
+          >1</button>
+          <button
+          className="btn-lg m-2"
+          style={{width: "100px"}}
+          onClick={{handleClickLimpiar}}
+          id="colorojo"
+          >Limpiar</button><br />
+          <button
+            className="btn-lg"
+            style={{width: "100px"}}
+            value={0}
+            onClick={handleNumeroClick}
+            id="colorazul"
+          >0</button>
+          
+         
+          
+          
+        </div>
       </div>
       
-    </>
-  )
+    </div>
+    </center>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
